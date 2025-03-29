8411d70f9f3bee659d886cba12b7f25d362d4486
Ondřej Lenikus <ondralenikus1@seznam.cz>
2025-03-29 13:26:15 +0100

    Maybe performance idk tbh

--- src/main/java/com/happysg/radar/block/radar/behavior/RadarScanningBlockBehavior.java
+++ src/main/java/com/happysg/radar/block/radar/behavior/RadarScanningBlockBehavior.java
@@ -70,7 +70,7 @@ public class RadarScanningBlockBehavior extends BlockEntityBehaviour {
         Level level = blockEntity.getLevel();
         if (level == null) return;
         for (Entity entity : scannedEntities) {
-            if (entity.isAlive() && isInFovAndRange(entity.position())) {
+            if (entity.isAlive() && isInFovAndRange(entity.position()) && !radarTracks.containsKey(entity.getUUID().toString())) {
                 RadarTrack track = new RadarTrack(entity);
                 radarTracks.put(track.id(), track);
                 if (entity instanceof Projectile)
